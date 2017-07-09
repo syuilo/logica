2958dc3a9f20048c5c90e29583f27af99b155609
syuilo <syuilotan@yahoo.co.jp>
2017-07-09 21:00:09 +0900

    :art:

--- src/web/button-tag.ts
+++ src/web/button-tag.ts
@@ -5,11 +5,12 @@ export default class ButtonTag extends NodeTag {
 	node: Button;
 
 	constructor(draw, tags, node) {
-		super(draw, tags, node)
+		super(draw, tags, node);
 
-		const button = this.el.rect(32, 32).move(16, 16).fill('#f00');
+		const button = this.el.rect(32, 32).move(16, 16).fill('#0f3a35').radius(2);
 		button.click(() => {
 			this.node.click();
+			button.fill(this.node.getState() ? '#ef4625' : '#0f3a35');
 		});
 	}
 }

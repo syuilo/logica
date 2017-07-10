41500adc1f63c4f0f6e00cc5b79411a9e8cc2f85
syuilo <syuilotan@yahoo.co.jp>
2017-07-10 17:40:42 +0900

    :art:

--- src/web/node-tags/led.ts
+++ src/web/node-tags/led.ts
@@ -4,7 +4,7 @@ export default class LedTag extends NodeTag {
 	constructor(draw, tags, node) {
 		super(draw, tags, node, 64, 64);
 
-		const led = this.el.rect(48, 48).move(8, 8).fill('#0c1319').radius(2);
+		const led = this.el.rect(48, 48).move(8, 8).fill('#0c1319').radius(2).style('pointer-events: none;');
 
 		node.on('input-updated', state => {
 			led.fill(state ? '#efcb34' : '#0c1319');

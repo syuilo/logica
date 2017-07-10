b009b6482b7612b5ac6f721e23b78eda1845667b
syuilo <syuilotan@yahoo.co.jp>
2017-07-11 00:24:16 +0900

    :art:

--- src/web/node-tags/package.ts
+++ src/web/node-tags/package.ts
@@ -4,8 +4,14 @@ import NodeTag from '../node';
 export default class PackageTag extends NodeTag {
 	node: Package;
 
-	constructor(draw, tags, node) {
-		super(draw, tags, node, 96, 96);
+	constructor(draw, tags, node: Package) {
+		let height = 96;
+
+		const inOutMax = Math.max((node.inputInfo || []).length, (node.outputInfo || []).length);
+
+		if (inOutMax > 5) height += (32 * (inOutMax - 5));
+
+		super(draw, tags, node, 96, height);
 
 		this.el.text(this.node.packageName).fill('#fff').move(10, 4);
 	}

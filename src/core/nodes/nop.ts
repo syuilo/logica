75abb3f0c8015f4441eff521fb9e6ce60addc19e
syuilo <syuilotan@yahoo.co.jp>
2017-07-11 19:02:15 +0900

    Improve nop gate description

--- src/core/nodes/nop.ts
+++ src/core/nodes/nop.ts
@@ -2,7 +2,7 @@ import のーど from '../node';
 
 export default class Nop extends のーど {
 	type = 'Nop';
-	desc = 'No operation. This gate will nothing to do.';
+	desc = 'No operation. This gate will nothing to do.\n何もしません。ただ入力された信号をそのまま出力するだけです。信号は他のゲート同様に1tick遅れるため、意図的に信号を遅延させたい場合に有用です。';
 
 	inputInfo = [{
 		id: 'x',

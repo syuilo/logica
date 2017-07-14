65327151550c7a481e823387cef6e00a3b03c3b0
syuilo <syuilotan@yahoo.co.jp>
2017-07-15 05:35:51 +0900

    Refactor

--- src/core/circuit.ts
+++ src/core/circuit.ts
@@ -17,6 +17,8 @@ export default class Circuit {
 
 	private shouldUpdates: Set<のーど> = new Set();
 
+	private previousStatesList: any[] = [];
+
 	constructor(nodes?: のーど[]) {
 		if (nodes) nodes.forEach(n => this.addNode(n));
 	}
@@ -34,6 +36,29 @@ export default class Circuit {
 	 * 回路の状態を1ステップ進めます
 	 */
 	private _tick() {
+		/**********************************************************
+		 * ここで各々のノードの状態を更新しますが、各々のノードが入力を
+		 * 逐一取得すると、このtick時点での本来の入力とは違った入力状態が
+		 * 取得されることになってしまう場合があり(例えばA,Bのノードがあり、
+		 * B の入力が A の出力に繋がれているような回路の場合を考えると、
+		 * tick開始時点での A の出力が HIGH だとしたらこのtickにおいては
+		 * B から見た入力は HIGH として取得されなければなりませんが、
+		 * もし A を更新した時に A の出力が LOW になった場合、
+		 * 次(といっても同じtick中)に B を更新した時 B が現在の A の
+		 * 出力状態を見てしまうので本来ならHIGHが取得されるべきところを
+		 * LOW として取得してしまうようなことが起きます。
+		 * これは例えると画像のガウスブラーを計算するときと似ていて、
+		 * ブラーをかけるとき各々のピクセルについて周囲の(近傍の)ピクセル
+		 * の平均の色を適用することになりますが、もしピクセルを参照する時に
+		 * 既に処理済みのピクセルを採用してしまうと結果がおかしくなります。
+		 * これを防ぐには参照するピクセルを処理前の画像のものにすれば
+		 * 良いわけです。それと同じです)、記憶しておいたtick開始前時点での
+		 * 入力状態を渡して、それに基づいて自身の状態を更新してもらうようにします。
+		 * 記憶しておいた出力状態なら、各々のノードを更新して捌いていく中で
+		 * ノードの出力状態が変わってもそれ(記憶)は影響を受けることがありません
+		 * ので、どんな順番でtick内の更新すべきノードを捌いていっても結果が
+		 * 変わらないようにできます。
+		 **********************************************************/
 
 		/**********************************************************
 		 1 全てのノードの現在の入力を記憶しておく
@@ -52,34 +77,14 @@ export default class Circuit {
 
 		/**********************************************************
 		 2 更新をリクエストされているノードを捌く
-		 * ここで各々のノードを更新しますが、各々のノードが入力を逐一取得すると
-		 * このtick時点での本来の入力とは違った入力状態が取得されることになって
-		 * しまうので(例えばA,Bのノードがあり、Bの入力がAの出力に繋がれている
-		 * 場合を考えると、tick開始時点でのAの出力がHIGHだとしたらこのtick
-		 * においてはBの入力はHIGHにならなければなりませんが、もしAを更新した
-		 * 時にAの出力がLOWになった場合、次(といっても同じtick中)にBを
-		 * 更新した時Bが現在のAの出力状態を見てしまうので本来ならHIGHが取得
-		 * されるべきところをLOWとして取得してしまうようなことが起きます。
-		 * これは例えると画像のガウスブラーを計算するときと似ていて、ブラーを
-		 * かけるとき各々のピクセルについて周囲の(近傍の)ピクセルの平均の色を
-		 * 適用することになりますが、もしピクセルを参照する時に既に処理済みの
-		 * ピクセルを採用してしまうと結果がおかしくなります。これを防ぐには
-		 * 参照するピクセルを処理前の画像のものにすれば良いわけです)、
-		 * 1で記憶しておいたtick開始前時点での入力状態を渡して、
-		 * それに基づいて自分の状態を更新してもらうようにします。
-		 * 記憶しておいた出力状態なら、各々のノードを更新して捌いていく中で
-		 * ノードの出力状態が変わってもそれ(記憶)は影響を受けることがありません
-		 * ので、どんな順番でtick内の更新すべきノードを捌いていっても結果が
-		 * 変わらないようにできます。
 		 **********************************************************/
 
-		const updatedNodes = [];
-
-		Array.from(new Set(this.shouldUpdates)).forEach((node, i) => {
-			this.shouldUpdates.delete(node);
-			node.update(inputs[i]);
-			updatedNodes.push(node);
-		});
+		const updatedNodes = Array.from(this.shouldUpdates)
+			.map((node, i) => {
+				this.shouldUpdates.delete(node);
+				node.update(inputs[i]);
+				return node;
+			});
 
 		/**********************************************************
 		 3 更新したノードが出力を変化させた場合、
@@ -88,18 +93,31 @@ export default class Circuit {
 		 **********************************************************/
 
 		updatedNodes.forEach(node => {
-			if (node.hasOutputPorts) {
-				node.outputInfo.forEach(o => {
-					if ((node as any).hasOwnProperty('_previousStates') && (node as any)._previousStates[o.id] === node.getState(o.id)) return;
-					if (!(node as any).hasOwnProperty('_previousStates')) (node as any)._previousStates = {};
-					(node as any)._previousStates[o.id] = node.getState(o.id);
+			if (!node.hasOutputPorts) return;
 
-					// このノードの出力として繋がれている全ての
-					// ノードを取得して次回の更新予定リストに登録
-					const next = node.getActualNextNodes(o.id);
-					next.forEach(n => this.shouldUpdates.add(n));
-				});
-			}
+			const previousStates = this.previousStatesList
+				.find(pss => pss.node == node)
+				.states;
+
+			node.outputInfo.forEach(o => {
+				// 前回の状態
+				const previousState = previousStates[o.id];
+
+				// 現在の状態
+				const currentState = node.getState(o.id)
+
+				// 前回の状態から変化がなかったら更新は発生させない
+				if (currentState === previousState) {
+					return
+				} else {
+					previousStates[o.id] = currentState;
+				}
+
+				// このノードの出力として繋がれている全ての
+				// ノードを取得して次回の更新予定リストに登録
+				const next = node.getActualNextNodes(o.id);
+				next.forEach(n => this.shouldUpdates.add(n));
+			});
 		});
 	}
 
@@ -120,7 +138,10 @@ export default class Circuit {
 	 */
 	public reset() {
 		const dive = node => {
-			delete node._previousStates;
+			this.previousStatesList
+				.find(pss => pss.node == node)
+				.states = {};
+
 			node.init();
 			node.emit('state-updated');
 			node.emit('input-updated');
@@ -146,7 +167,14 @@ export default class Circuit {
 
 	private scan(node: のーど) {
 		node.requestUpdateAtNextTick = () => this.shouldUpdates.add(node);
+
+		this.previousStatesList.push({
+			node: node,
+			states: {}
+		});
+
 		if (node.isForceUpdate) this.shouldUpdates.add(node);
+
 		if (node.type === 'Package') {
 			(node as Package).nodes.forEach(n => {
 				this.scan(n);

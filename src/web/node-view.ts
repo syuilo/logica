fdba96e8c2c5bedf4941fdc5fede4f5769f723c6
こぴなたみぽ <syuilotan@yahoo.co.jp>
2017-12-25 10:41:16 +0900

    wip

--- src/web/node-view.ts
+++ src/web/node-view.ts
@@ -6,14 +6,91 @@ import { Connection } from '../core/node';
 import Package from '../core/nodes/package';
 
 import NodesView from './nodes-view';
-import NodeViewModel from './node-view-model';
 import Config from './config';
+
+@autobind
+export abstract class NodeViewModel extends EventEmitter {
+	id: string;
+
+	node: のーど;
+
+	private _x: number;
+	private _y: number;
+
+	get x() {
+		return this._x;
+	}
+
+	get y() {
+		return this._y;
+	}
+
+	set x(x) {
+		this._x = x;
+	}
+
+	set y(y) {
+		this._y = y;
+	}
+
+	/**
+	 * この値を操作しないでください
+	 *
+	 * 0 ... ↑ (default)
+	 * 1 ... →
+	 * 2 ... ↓
+	 * 3 ... ←
+	 */
+	public rotate: number = 0;
+
+	inputPorts: any[] = [];
+	outputPorts: any[] = [];
+
+	config: Config;
+
+	constructor(config: Config, node: のーど) {
+		super();
+
+		this.setMaxListeners(Infinity);
+
+		this.config = config;
+		this.node = node;
+	}
+
+
+	/**
+	 * 移動します
+	 * @param x X位置
+	 * @param y Y位置
+	 */
+	public move(x: number, y: number) {
+		if (this.config.snapToGrid) {
+			const gridSize = 16;
+			x = Math.round(x / gridSize) * gridSize;
+			y = Math.round(y / gridSize) * gridSize;
+		}
+
+		if (x !== this.x || y !== this.y) {
+			this.emit('moved');
+		}
+	}
+
+	/**
+	 * 回転します
+	 * @param r 0~4
+	 */
+	public setRotate(r: number) {
+		if (r > 3) r = 0;
+		this.rotate = r;
+		this.emit('moved');
+	}
+}
 
 // ポートの直径
 const diameter = 8;
 
 @autobind
-abstract class NodeView extends EventEmitter {
+export abstract class NodeView extends EventEmitter {
 	viewModel: NodeViewModel;
 	el: any;
 
@@ -323,8 +400,6 @@ abstract class NodeView extends EventEmitter {
 		if (this.mouseoutTimer) clearTimeout(this.mouseoutTimer);
 	}
 }
-
-export default NodeView;
 
 /**
  * 導線
--- src/web/node-views/and.ts
+++ src/web/node-views/and.ts
@@ -1,16 +1,22 @@
 import NodesView from '../nodes-view';
-import NodeView from '../node-view';
+import { NodeView, NodeViewModel } from '../node-view';
 import Config from '../config';
 import And from '../../core/nodes/and';
 
 export default class AndView extends NodeView {
-	constructor(config: Config, nodesView: NodesView, node?: And) {
-		super(config, nodesView, node || new And(), 64, 64);
+	constructor(config: Config, nodeViewModel: NodeViewModel, node?: And) {
+		super(config, nodeViewModel, 64, 64);
 
 		this.el.text('And').fill('#fff').style('pointer-events: none;').move(10, 4);
 	}
+}
 
-	public static import(config: Config, nodesView: NodesView, data) {
-		return new AndView(config, nodesView, And.import(data.node));
+export default class AndViewModel extends NodeViewModel {
+	constructor(config: Config, node?: And) {
+		super(config, node || new And());
+	}
+
+	public static import(config: Config, data) {
+		return new AndViewModel(config, And.import(data.node));
 	}
 }
